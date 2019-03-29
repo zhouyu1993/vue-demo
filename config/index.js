fe0ae214bf28cfccbc01b2e998c428f4b435c749
zhouyu1993 <1490079545@qq.com>
2019-03-29 16:39:55 +0800

    update

--- config/index.js
+++ config/index.js
@@ -79,7 +79,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '//zhouyu1993.github.io/awesome/vue-demo/',
+    assetsPublicPath: '/awesome/vue-demo/',
 
     /**
      * Source Maps
